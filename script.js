e7cad6b9e3fd1632ddef2aff10007d8203ed097e
Juan1Mera <98964201+Juan1mera@users.noreply.github.com>
2024-05-16 20:41:18 -0500

    Add files via upload
    Modificacion del estilo

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Obtener la columna derecha
-var rightColumn = document.getElementById('right-column');
+// Obtén el botón de idioma y el submenú de idioma
 
-// Escuchar el evento de scroll en la ventana
-window.addEventListener('scroll', function() {
-    // Si el usuario ha desplazado más allá de la mitad de la ventana
-    if (window.scrollY > (window.innerHeight / 2)) {
-        // Agregar la clase .expanded a la columna derecha
-        rightColumn.classList.add('expanded');
-    } else {
-        // Si no, quitar la clase .expanded
-        rightColumn.classList.remove('expanded');
-    }
-});
+
+
+
