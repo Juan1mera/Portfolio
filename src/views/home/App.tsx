2f673b28775393d11799ddf84e54d6f620db507d
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-14 08:23:12 -0500

    style: Agregando un dock al home

--- src/views/home/App.tsx
+++ src/views/home/App.tsx
@@ -1,16 +1,23 @@
-import TextPressure from '../../components/TextPressure'
-import Projects from '../projects/Projects'
+import Dock from '../../components/Dock/Dock'
 import './App.css'
-
+import { VscHome,VscArchive, VscAccount, VscSettingsGear } from "react-icons/vsc";
 function App() {
+  const items = [
+    { icon: <VscHome size={18} />, label: 'About', onClick: () => alert('Home!') },
+    { icon: <VscArchive size={18} />, label: 'Projects', onClick: () => alert('Archive!') },
+    { icon: <VscAccount size={18} />, label: 'Galery', onClick: () => alert('Profile!') },
+    { icon: <VscSettingsGear size={18} />, label: 'Contact', onClick: () => alert('Settings!') },
+  ];
 
+  
   return (
-  <div style={{position: 'relative', }}>
-    <TextPressure
-      text="Welcome!"
-      textColor="#edecee"
-    />
-    <Projects />
+  <div style={{position: 'fixed', bottom: 0}}>
+  <Dock 
+    items={items}
+    panelHeight={68}
+    baseItemSize={50}
+    magnification={70}
+  />
   </div>
   )
 }
