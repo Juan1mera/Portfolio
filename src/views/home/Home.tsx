626855de5ce7f10cfc546c86c7d5f7996c85bb8a
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-19 07:43:59 -0500

    fix: Cambiando el Animated por un Fade

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,6 +1,5 @@
 
-import AnimatedContent from '../../components/AnimatedContent'
-import DecryptedText from '../../components/DecryptedText'
+import FadeContent from '../../components/FadeContent'
 import TextPressure from '../../components/TextPressure'
 import '../../styles/global.css'
 import './Home.css'
@@ -8,22 +7,18 @@ import './Home.css'
 
 function Home() {
   return (
-    <AnimatedContent>
+    <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
     <div className='container'>
       <TextPressure text='Hello World!' />
       <div >
         <div style={{ marginTop: '4rem', maxWidth: '60ch' }}>
-        <DecryptedText
-          text="This text animates when in view , and when out of view lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          animateOn="view"
-          revealDirection="end"
-          speed={50}
-          maxIterations={10}
-        />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum magnam voluptatum recusandae minima perspiciatis? Illum vero laborum aspernatur veniam quis, quisquam corporis totam doloremque minima sed. Culpa perspiciatis quasi non! Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ipsam enim eius corporis alias quis corrupti! Cum minus tempora aut commodi minima impedit? Repellendus tempore, cupiditate ratione dolores laudantium molestiae.
+          </p>
         </div>
       </div>
     </div>|
-    </AnimatedContent>
+    </FadeContent>
 
   )
 }
