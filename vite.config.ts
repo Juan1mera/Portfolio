7903b5bfd7c984ad27fe7e9f7ece859d1c5df8ad
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 17:06:00 -0500

    add: Agregando Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
 
 // Obtener el equivalente de __dirname en ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
