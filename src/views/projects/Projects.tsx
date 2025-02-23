d38f83c95f1ace93129d07b9908b279d713c7bb4
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 17:53:00 -0500

    style: Agregando el infinite scroll

--- src/views/projects/Projects.tsx
+++ src/views/projects/Projects.tsx
@@ -1,7 +1,11 @@
+import { items } from "../../assets/proyects"
+import InfiniteMenu from "../../components/InfiniteMenu "
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div style={{ height: '600px', position: 'relative' }}>
+      <InfiniteMenu items={items}/>
+    </div>
   )
 }
 
