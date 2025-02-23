b47617cf0fe55d3105e9d0f1537aa2fe71a72f8f
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 17:06:14 -0500

    style: Estilos del RotatingText

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -14,12 +14,18 @@ function Home() {
       <div>
         <div className='gridContainer'>
           <div>
-            <h1>¡Hola! Soy Juan</h1>
+            <h1>¡Hola! Soy Juan!</h1>
             <RotatingText 
-              texts={['xxxx', 'dssss', 'sdddd']}
-              transition={{ duration: 0.8 }}
-              staggerDuration={0.09}
-              color='rgb(255, 255, 255)'
+              texts={['Software Engineer Student', 'Full Stack Developer', ':3']}
+              mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+              staggerFrom={"last"}
+              initial={{ y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-120%" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+              transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              rotationInterval={2000}
             />
           </div>
           <div>
