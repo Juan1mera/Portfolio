ba732f5e8951185f362ae02c9bb2a1bca492513c
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 18:04:19 -0500

    fix: Ahora ocupa todo el ancho

--- src/views/projects/Projects.tsx
+++ src/views/projects/Projects.tsx
@@ -3,9 +3,7 @@ import InfiniteMenu from "../../components/InfiniteMenu "
 
 function Projects() {
   return (
-    <div style={{ height: '600px', position: 'relative' }}>
       <InfiniteMenu items={items}/>
-    </div>
   )
 }
 
