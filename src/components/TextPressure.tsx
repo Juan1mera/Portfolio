59eade651e0f8c296aa94faba4c28c5ecd8b6b80
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 16:57:32 -0500

    style: Eliminando alto del componente

--- src/components/TextPressure.tsx
+++ src/components/TextPressure.tsx
@@ -163,7 +163,6 @@ const TextPressure: React.FC<TextPressureProps> = ({
             style={{
                 position: 'relative',
                 width: '100%',
-                height: '100%',
                 background: 'transparent',
             }}
         >
