6eca1b6a67cd251d51dc900aeeb0df3ff45d72e4
Juan1mera <juan1mera2barrera@gmail.com>
2025-02-23 17:25:14 -0500

    fix: Acomodando espacio y  textp

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -15,9 +15,10 @@ function Home() {
         <div className='gridContainer'>
           <div>
             <h1>¡Hola! Soy Juan!</h1>
+            <br />
             <RotatingText 
               texts={['Software Engineer Student', 'Full Stack Developer', ':3']}
-              mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+              mainClassName="px-2 sm:px-2 md:px-3 bg-[#534984] text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg font-bold"
               staggerFrom={"last"}
               initial={{ y: "100%" }}
               animate={{ y: 0 }}
@@ -25,12 +26,12 @@ function Home() {
               staggerDuration={0.025}
               splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
               transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={2000}
+              rotationInterval={4000}
             />
           </div>
           <div>
             <p>
-            Estudiante de ingeniería de software y desarrollador Full Stack en Colombia. Tengo 17 y especializo en crear aplicaciones funcionales sin comprometer la experiencia del usuario. Sin experiencia laboral pero con un fuerte deseo de seguir aprendiendo.
+            Soy estudiante de Ingeniería de Software y desarrollador Full Stack en Colombia. Con 17 años, me especializo en crear aplicaciones funcionales sin comprometer la experiencia del usuario. Aunque aún no cuento con experiencia laboral cuento con experiencia y varios proyectos personales, tengo un gran deseo de aprender y mejorar constantemente.
             </p>
           </div> 
         </div>
