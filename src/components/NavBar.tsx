7633c35095db63981d840c3241765c10d1e59060
Juan Mera <juan1mera2barrera@gmail.com>
2025-10-28 22:23:23 +0300

    feat: Agregando todas las secciones

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav>
+      <h5>Juan Mera</h5>
+      <div>
+        <a>Home</a>
+        <a>About</a>
+        <a>arojects</a>
+      </div>
+      <a href="">Let's Talk</a>
+    </nav>
   )
 }
 
---
+++ src/views/home/Home.tsx
@@ -0,0 +1,22 @@
+import './home.css'
+
+function Home() {
+  return (
+    <main>
+      <div>
+        <h5 className='p-2'>I'm a </h5>
+        <h1 className='font-extrabold' >FULL-STACK</h1>
+      </div>
+      <div>
+        <h1>DEVELOPER</h1>
+        <h5>with experience</h5>
+      </div>
+      <div>
+        <h5>in</h5>
+        <h1>WEB, APPS & GAMING</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Home
