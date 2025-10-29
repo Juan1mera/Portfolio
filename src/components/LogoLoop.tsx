7d34aa2798556b0fb0017d3e4eb54d83e7297506
Juan Mera <juan1mera2barrera@gmail.com>
2025-10-29 12:37:51 +0300

    fix: Centrado de alguna manera el texto ctm

--- src/components/LogoLoop.tsx
+++ src/components/LogoLoop.tsx
@@ -284,7 +284,7 @@ export const LogoLoop = React.memo<LogoLoopProps>(
               )}
             >
               {content}
-              <span className="text-[0px] group-hover:text-sm duration-300 font-medium">
+              <span className="text-[0px] group-hover:text-sm duration-300 font-bold pb-2.5">
                 {hoverText}
               </span>
             </div>
