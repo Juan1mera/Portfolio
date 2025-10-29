385c1db32e96d08dde47045d8377b7f88bdce994
Juan Mera <juan1mera2barrera@gmail.com>
2025-10-29 11:06:33 +0300

    style: Agregando menu de trabajos

--- src/views/work/Work.tsx
+++ src/views/work/Work.tsx
@@ -1,7 +1,19 @@
+import FlowingMenu from "../../components/FlowingMenu";
 
 function Work() {
+
+  const demoItems = [
+    { link: '#', text: 'Mojave', image: 'https://picsum.photos/600/400?random=1' },
+    { link: '#', text: 'Sonoma', image: 'https://picsum.photos/600/400?random=2' },
+    { link: '#', text: 'Monterey', image: 'https://picsum.photos/600/400?random=3' },
+    { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }
+  ];
+
+
   return (
-    <div>Work</div>
+  <div style={{ height: '600px', position: 'relative' }}>
+    <FlowingMenu items={demoItems} />
+  </div>
   )
 }
 
