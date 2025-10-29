29143cbed7c48bc120cf8dbbb4637235b4c8c48f
Juan Mera <juan1mera2barrera@gmail.com>
2025-10-29 14:45:21 +0300

    style: Vista de efecto al cargar la pagina

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,22 +1,66 @@
-import './home.css'
+import BlurText from "../../components/BlurText";
 
 function Home() {
   return (
-    <main>
-      <div>
-        <h5 className='p-2'>I'm a </h5>
-        <h1 className='font-extrabold' >FULL-STACK</h1>
+    <section
+      id="home"
+      className="min-h-screen flex flex-col justify-start items-center gap-6 pt-44 w-full px-6 md:px-0"
+    >
+      {/* Línea 1: Alineada a la IZQUIERDA */}
+      <div className="w-full px-20 flex items-end gap-3 flex-wrap justify-start">
+        <BlurText
+          text="I'm a"
+          className="p-2 md:text-6xl text-3xl "
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
+        <BlurText
+          text="FULL-STACK"
+          className="font-extrabold text-5xl md:text-9xl text-purple-light"
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
       </div>
-      <div>
-        <h1>DEVELOPER</h1>
-        <h5>with experience</h5>
+
+      {/* Línea 2: Alineada a la DERECHA */}
+      <div className="w-full px-20 flex items-end gap-3 flex-wrap justify-end">
+        <BlurText
+          text="DEVELOPER"
+          className="font-extrabold text-5xl md:text-9xl text-purple-light"
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
+        <BlurText
+          text="with experience"
+          className="text-3xl md:text-6xl "
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
       </div>
-      <div>
-        <h5>in</h5>
-        <h1>WEB, APPS & GAMING</h1>
+
+      {/* Línea 3: CENTRADA */}
+      <div className="w-full px-20 flex items-center gap-3 flex-wrap justify-center">
+        <BlurText
+          text="in"
+          className="text-3xl md:text-6xl "
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
+        <BlurText
+          text="WEB, APPS & GAMING"
+          className="font-extrabold text-5xl md:text-9xl "
+          animateBy="words"
+          delay={300}
+          direction="top"
+        />
       </div>
-    </main>
-  )
+    </section>
+  );
 }
 
-export default Home
+export default Home;
