804e4ed20defce2c979f74d67a56f1bc0c8bf137
Juan Mera <juan1mera2barrera@gmail.com>
2025-10-30 15:31:42 +0300

    style: Dandole estilos al contacto

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -1,7 +1,21 @@
+import { CustomTitleSection } from "../../components/CustomTitleSection"
+import CustomLink from "../../components/ui/CustomLink"
 
 function Contact() {
   return (
-    <div id="contact">Contact</div>
+    <section 
+      id="contact"
+      className="flex flex-col justify-start items-center py-40 gap-44 w-full"
+    >
+        <CustomTitleSection
+          topTexts={["LET'S", "BUILD"]}
+          bottomTexts={["SOMETHING"]}
+          purpleWord="BUILD"
+        />
+      <div className="min-w-screen flex items-center justify-center">
+        <CustomLink text="mera.dev.co@gmail.com" href="" />
+      </div>
+    </section>
   )
 }
 
